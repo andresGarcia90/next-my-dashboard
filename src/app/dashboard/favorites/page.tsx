aa9f749c852dd8bdb534dd0c0ddeb211b9d4b966
Andres Garcia <amgarciaamado@hipotecarioseguros.com>
2024-09-11 17:53:16 -0300

    remove log

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -8,16 +8,11 @@ export const  FavoritesPage = () => {
   const pokemons = useAppSelector( state => state.pokemons.favorites );
   const pokemonList = Object.values(pokemons);
 
-  console.log(pokemons);
-  
-
   return (
     <div className="flex flex-col">
     <div className="flex flex-wrap gap-10 items-center justify-center">
       {pokemonList.length === 0 && <p className="text-center">No hay favoritos</p>}
       {pokemonList.length > 0 && <PokemonGrid pokemons={pokemonList} />}
-      
-      
     </div>
   </div>
   )
